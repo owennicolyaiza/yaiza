edf668211dae5dc07ffbf8d9e9fd91fc7b46ed28
Owen Nicol <owen.nicol@johnlewis.co.uk>
2020-05-29 07:15:22 +0100

    Add logs to wakeDyno

--- wakeDyno.js
+++ wakeDyno.js
@@ -6,6 +6,8 @@ const DYNO_URL = 'https://yaiza-site-new.herokuapp.com';
 const server = express();
 
 server.listen(port, err => {
+  console.log('====> wakeDyno running on port:', port);
+  console.log('====> err:', err);
   if (err) throw err;
   wakeDyno(DYNO_URL).start();
 });
