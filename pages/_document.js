68deb4f878085ba48d467c65fd71ea7fc2ac8ddc
Owen Nicol <owennicol@users.noreply.github.com>
2024-06-12 10:18:56 +0100

    Add Google Analytics tags

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,23 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import React from 'react'
+
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-RER51CHJTT"></script>
+          <script dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || []
+            function gtag(){window.dataLayer.push (arguments)}
+            gtag('js', new Date ());
+            gtag ('config', 'G-RER51CHJTT');
+             `,
+          }}
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
